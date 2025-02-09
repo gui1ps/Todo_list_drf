90cfdb715f09973066588c6a67083940f3bd3c54
Guilherme Roberto dos Passos <115484995+gui1ps@users.noreply.github.com>
2025-02-09 12:04:35 -0300

    Refactor task retrieval logic to use a helper function for instance verification

--- todo/views.py
+++ todo/views.py
@@ -4,6 +4,12 @@ from rest_framework import status
 from todo.models import Todo
 from todo.serializers import TodoSerializer
 
+def verify_instance(pk):
+    try:
+        return Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return None
+
 @api_view(['GET'])
 def get_tasks(request):
     all_tasks=Todo.objects.all()
@@ -20,18 +26,16 @@ def create_task(request):
 
 @api_view(['GET'])
 def get_task(request,pk):
-    try:
-        task=Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
+    if not verify_instance(pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
+    task=Todo.objects.get(pk=pk)
     tasks_serializer=TodoSerializer(task)
     return Response(tasks_serializer.data,status=status.HTTP_200_OK)
 
 @api_view(['DELETE'])
 def delete_task(request,pk):
-    try:
-        task=Todo.objects.get(pk=pk)
-    except Todo.DoesNotExist:
+    if not verify_instance(pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
+    task=Todo.objects.get(pk=pk)
     task.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
